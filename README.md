37ccb4eb9dcbb699e3ce3dc6942e66e8b7908b67
Anders Gustafsson <anders@photowall.se>
2020-02-16 09:26:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# simple-sl-api
+A simple SL API wrapper to keep api secrets out of client.
+
+WIP - Currently supports SL Realtidsinformation 4 API only.
---
+++ routes/nextDeparture.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const key = process.env.REALTIME_KEY;
+
+router.get('/:siteId', async function(req, res, next) {
+    const siteId = req.params.siteId;
+    const timewindow = req.query.timewindow;
+    const apiUrl = `https://api.sl.se/api2/realtimedeparturesV4.json?key=${key}&timewindow=${timewindow}&siteid=${siteId}`;
+    try {
+        const response = await axios.get(apiUrl);
+        if (response.data.StatusCode !== 0) {
+            throw new Error(response.data.Message);
+        }
+        res.json(response.data);
+    } catch(error) {
+        console.error('Failed fetching real time departures: ', error.message);
+        next(error);
+    }
+});
+
+module.exports = router;
